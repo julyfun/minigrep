9f1cafaf18156830bc5c486e415916b4a119869c
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-31 17:37:07 +0800

    refactor: 分离 main 和 lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use minigrep::{run, Config};
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -11,27 +11,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("found text:\n{contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Self { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
