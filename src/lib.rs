cee4569e5e9667c82ff920a4916b9dcf99a2b116
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-31 17:50:51 +0800

    feat: search 函数

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 use std::error::Error;
 use std::fs;
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     println!("found text:\n{contents}");
@@ -20,5 +30,20 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Self { query, file_path })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+C++:
+unsafe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["unsafe, fast, productive."], search(query, contents));
     }
 }
