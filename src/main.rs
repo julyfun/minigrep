c9e3b06ec8620faae2875fe01deac444828f102c
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-31 16:51:54 +0800

    feat: Config 数据抽象

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
-    println!("Searching \"{query}\" in file {file_path}");
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Failed to read file.");
+    let contents = fs::read_to_string(config.file_path).expect("Failed to read file.");
     println!("found text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
