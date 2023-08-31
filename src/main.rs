16a51714efe2be8018b36e450e16cc5a4165c205
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-31 16:46:55 +0800

    feat: 基本功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
+    println!("Searching \"{query}\" in file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Failed to read file.");
+    println!("found text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
