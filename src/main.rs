95147572efb0fe190e3ed50ff97f17b75e4a5b8e
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-31 17:17:15 +0800

    fix: Config::new 把 panic 换成 Err

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem is: {err}");
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
@@ -16,12 +20,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Self { query, file_path }
+        Ok(Self { query, file_path })
     }
 }
